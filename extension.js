afc96ae70d56663c6ac62da62e7a4ad9599b307c
crashoffnet <119506547+crashoffnet@users.noreply.github.com>
2022-11-30 18:16:54 +0300

    Bug fixes

--- extension.js
+++ extension.js
@@ -202,7 +202,7 @@
 
             if (counterText.startsWith('Starting in')) {
                 status = 'timer'
-                counter = (Math.floor(parseFloat(counterText.replace('Starting in 0', '')) * 10) / 10).toFixed(1) + 's'
+                counter = (Math.floor(parseFloat(counterText.replace('Starting in 0', '').replace('s', '')) * 10) / 10).toFixed(1) + 's'
             } else if (counterText.startsWith('Crashed at')) {
                 status = 'crash'
                 counter = counterText.replace('Crashed at x', '') + 'x'
